2cfda82769992f8a0d726bda4aed8f3e28d1ce77
Juan Villegas <jvillegas422@gmail.com>
2022-10-22 14:47:59 -0500

    Update GalleryList component

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,9 +1,19 @@
-function GalleryList() {
-    console.log('in GalleryList.jsx');
+import GalleryItem from "../GalleryItem/GalleryItem";
+
+function GalleryList([galleryItems]) {
+    console.log('in GalleryList.jsx', galleryItems);
     
     return (
         <>
             <h2>GalleryList</h2>
+            <div>
+                {galleryItems.map(imgs => 
+                    (<GalleryItem
+                        key={imgs.id}
+                        imgs={imgs}
+                    />)
+                )};
+            </div>
         </>
     );
 }

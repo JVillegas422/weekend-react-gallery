c8236cec60068ee1fcb00f93c5945a0b73fd04f5
Juan Villegas <jvillegas422@gmail.com>
2022-10-22 13:29:58 -0500

    Add GalleryList & GalleryItem component base setup

---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,11 @@
+function GalleryItem() {
+    console.log('in GalleryItem.jsx');
+
+    return (
+        <>
+            <h2>GalleryItem</h2>
+        </>
+    );
+}
+
+export default GalleryItem;
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,11 @@
+function GalleryList() {
+    console.log('in GalleryList.jsx');
+    
+    return (
+        <>
+            <h2>GalleryList</h2>
+        </>
+    );
+}
+
+export default GalleryList;

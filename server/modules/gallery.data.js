b27ca9c28483e77a58dde7513b701bbf0fbe7982
Juan Villegas <jvillegas422@gmail.com>
2022-10-23 15:05:40 -0500

    Update css text color

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,7 @@
 const galleryItems = [
     { id: 1, path: 'images/pushAnyways.jpg', description: 'Build is broken. Push anyways.', likes: 0 },
     { id: 2, path: 'images/bug_meme.jpg', description: 'We find a bug, we fix, now we have two bugs.', likes: 0 },
-    { id: 3, path: 'images/knowHowToFix.jpg', description: 'Hey, are you sleeping? Yes, now shut up. I know how to fix that bug on line 255..', likes: 0 },
+    { id: 3, path: 'images/knowHowToFix.jpg', description: 'Hey, are you sleeping? I know how to fix that bug on line 255..', likes: 0 },
     { id: 4, path: 'images/worst_code.jpg', description: 'Yours is the worst code I\'ve ever run...but it runs.', likes: 0 },
     { id: 5, path: 'images/zeroOrNull.jpg', description: '0 vs Null.', likes: 0 },
     { id: 6, path: 'images/techDebt.jpg', description: 'Don\'t worry about this tech debt, we\'ll clean it up next sprint.', likes: 0 },
